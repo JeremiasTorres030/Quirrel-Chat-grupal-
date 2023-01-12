e2ee94113caa7495b0eeba37bf75319d4eef6835
JeremiasTorres030 <jeremiastorres030@gmail.com>
2023-01-12 17:24:56 -0300

    .Env updated

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -1,10 +1,9 @@
 import mongoose from 'mongoose'
+import { MONGO_URI } from '../env.config'
 
 export const connectDB = async () => {
   try {
-    const db = await mongoose.connect(
-      'mongodb+srv://Jeremias:d0K8cdKddJWmF5Ya@cluster0.bhekfst.mongodb.net/?retryWrites=true&w=majority'
-    )
+    const db = await mongoose.connect(MONGO_URI())
     console.log('se conecto a la base de datos ' + db.connection.name)
   } catch (error) {
     console.log(error)
--- server/src/env.config.ts
+++ server/src/env.config.ts
@@ -6,6 +6,12 @@ export const SECRET_JWT_SEED = (): string => {
   if (process.env.SECRET_JWT_SEED !== undefined) {
     return process.env.SECRET_JWT_SEED
   }
+  return ''
+}
 
+export const MONGO_URI = (): string => {
+  if (process.env.MONGO_URI !== undefined) {
+    return process.env.MONGO_URI
+  }
   return ''
 }
